436073513322f76525bca7a7b2f4ce22803a3f47
unknown <jaimematas88@gmail.com>
2023-11-16 20:07:01 +0100

    Seccion8 161123

---
+++ Seccion8/Notas.md
@@ -0,0 +1,6 @@
+## Seccion 8: SpringMVC & Data JPA
+
+En esta seccion se revisan:
+- ...
+
+Ultima leccion pracicada :point_rigth: 116
---
+++ Seccion8/spring-boot-seccion8-DataJPA/src/main/java/spring/boot/seccion8/datajpa/app/SpringBootSeccion8DataJpaApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.seccion8.datajpa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeccion8DataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSeccion8DataJpaApplication.class, args);
+	}
+
+}
