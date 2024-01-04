26e25d9e61df7f26ab2668c5380eb7cc4d6c2d24
JaimeMatasAsensio <jaimematas88@gmail.com>
2024-01-04 11:37:05 +0100

    Seccion2 040124

---
+++ Seccion1/SpringMVC/src/test/java/com/cursov3/springboot/seccion2/SpringMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursov3.springboot.seccion2.SpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Seccion2/Notas.md
@@ -0,0 +1,6 @@
+## Seccion 1: New Spring Boot 3 - Spring MVC
+Contenidos de la seccion:
+
+- ...
+
+Seccion 2: Siguiente leccion 11
